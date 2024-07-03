26b8e6676990b5c62e7c99ddc18c0840bfaa6196
Satyanchal <62104921+iamsatyanchal@users.noreply.github.com>
2024-07-03 22:20:29 +0530

    Create popup.js

---
+++ popup/popup.js
@@ -0,0 +1,5 @@
+document.getElementById('toggleButton').addEventListener('click', () => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "toggle"});
+  });
+});
